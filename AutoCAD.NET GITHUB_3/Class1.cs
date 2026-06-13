cfdff04b7afdfdfb186cb3f76557800ffd92038c
Công Nguyễn <thanhcongspktxd@gmail.com>
2026-06-13 20:03:17 +0700

    Added Command SafeUserLine

--- AutoCAD.NET GITHUB_3/Class1.cs
+++ AutoCAD.NET GITHUB_3/Class1.cs
@@ -11,7 +11,69 @@ namespace AutoCAD.NET_GITHUB_3
 {
     public class Class1
     {
+        [CommandMethod("SAFEUSERLINE")]
+        public void SafeUserLine()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
 
+            // Điểm đầu
+
+            PromptPointOptions ppo1 = new PromptPointOptions("\nChọn điểm đầu: ");
+
+            PromptPointResult ppr1 = ed.GetPoint(ppo1);
+
+            if (ppr1.Status != PromptStatus.OK)
+            {
+                return;
+            }
+                
+
+            Point3d pt1 = ppr1.Value;
+
+            // Điểm cuối
+
+            PromptPointOptions ppo2 = new PromptPointOptions("\nChọn điểm cuối: ");
+
+            ppo2.BasePoint = pt1;
+
+            ppo2.UseBasePoint = true;
+
+            PromptPointResult ppr2 = ed.GetPoint(ppo2);
+
+            if (ppr2.Status != PromptStatus.OK)
+            {
+                return;
+            }
+
+            Point3d pt2 = ppr2.Value;
+
+            // Validate
+
+            if (pt1.IsEqualTo(pt2))
+            {
+                ed.WriteMessage(
+                    "\nHai điểm không được trùng nhau!");
+
+                return;
+            }
+
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId,OpenMode.ForRead);
+
+                BlockTableRecord btr = (BlockTableRecord)tr.GetObject(bt[BlockTableRecord.ModelSpace],OpenMode.ForWrite);
+
+                Line line = new Line(pt1,pt2);
+
+                btr.AppendEntity(line);
+
+                tr.AddNewlyCreatedDBObject(line, true);
+
+                tr.Commit();
+            }
+        }
 
 
     }

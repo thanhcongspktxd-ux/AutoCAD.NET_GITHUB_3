0acc089d46bde4339c40d896ecc385f33f65d209
Công Nguyễn <thanhcongspktxd@gmail.com>
2026-06-13 19:36:02 +0700

    Added Command Countentity

--- AutoCAD.NET GITHUB_3/Class1.cs
+++ AutoCAD.NET GITHUB_3/Class1.cs
@@ -11,7 +11,32 @@ namespace AutoCAD.NET_GITHUB_3
 {
     public class Class1
     {
+        [CommandMethod("COUNTENTITY")]
+        public void CountEntity()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
 
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId,OpenMode.ForRead);
+
+                BlockTableRecord btr = (BlockTableRecord)tr.GetObject(bt[BlockTableRecord.ModelSpace],OpenMode.ForRead);
+
+                int count = 0;
+
+                foreach (ObjectId id in btr)
+                {
+                    count++;
+                }
+
+                ed.WriteMessage(
+                    $"\nCó {count} đối tượng trong ModelSpace");
+
+                tr.Commit();
+            }
+        }
 
 
     }
